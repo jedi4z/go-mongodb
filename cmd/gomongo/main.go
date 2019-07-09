9f5a88369c03690a27339c38c78cc87433ce12e7
Jesús Díaz <jesusdiazbc@gmail.com>
2019-07-09 13:38:01 -0300

    Add main

--- cmd/gomongo/main.go
+++ cmd/gomongo/main.go
@@ -1,1 +1,18 @@
-package gomongo
+package main
+
+import (
+	"github.com/jedi4z/go-mongodb/app/interface/rest"
+	"github.com/jedi4z/go-mongodb/app/registry"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	ctn, err := registry.NewContainer()
+	if err != nil {
+		log.Fatalf("failed to build container: %v", err)
+	}
+
+	engine := rest.NewEngine(ctn)
+
+	engine.Run()
+}
